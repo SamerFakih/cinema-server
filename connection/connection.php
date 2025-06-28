9446cc93bf2ffcbf52463b8e12f767a3a1e86f4d
abosmraa <samerfakih15@gmail.com>
2025-06-28 16:57:59 +0300

    added static connection

--- connection/connection.php
+++ connection/connection.php
@@ -7,3 +7,9 @@ $pass = '';
 
 
 $conn= new mysqli($host, $user, $pass, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+Model::setConnection($conn);
+
